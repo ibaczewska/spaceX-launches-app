d1b4607b9a45b2688cb5210aae4d45f118b15ecf
ibaczewska <izabela.baczewska@gmail.com>
2018-05-04 19:58:42 +0200

    left-column content

--- src/view/LaunchDetails.js
+++ src/view/LaunchDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LaunchDetails.sass';
 import logo from '../assets/space_x_logo_bw_centered.png';
+import itemLogo from '../assets/item-logo.png';
 
 class LaunchDetails extends React.Component {
     constructor() {
@@ -12,36 +13,42 @@ class LaunchDetails extends React.Component {
     
 
         return (
-            <div className='container'>
-                <div className='header'>
-                    <span className='header-arrow'> &nbsp;Go back</span>
-                    <img className='header-logo' src={logo} width='200' />
+            <div className="container">
+                <div className="header">
+                    <span className="header-arrow"> &nbsp;Go back</span>
+                    <img className="header-logo" src={logo} width='200' />
                     <div></div>
                 </div>
-                <div className='content'>
-                    <div className='content-left'>
-                        <p>aaaaa  </p> 
+                <div className="content">
+                    <div className="content-left item">
+                        <div className="item-date">26 may 2018</div> 
+                        <div className="item-title">Falcon 9</div>
+                        <div className="item-timer">20 days 11 hrs 34 mins to start</div>
+                        <div className="item-logo">
+                            <img src={itemLogo} width='150'/>
+                        </div>
                     </div>
-                    <div className='content-right'>
+                    <div className="content-right">
                         <p>bbbbb</p>
                     </div>
                 </div>
                 
-                <div className='links-wrapper'>
-                    <h4 className='links-title'>Mission Links</h4>
-                    <a href={'#'} className='links-button'>reddit campaign</a>
-                    <a href={'#'} className='links-button'>presskit</a>
-                    <a href={'#'} className='links-button'>mission video</a>
+                
+                <div className="links-wrapper">
+                    <h4 className="links-title">Mission Links</h4>
+                    <a href={'#'} className="links-button">reddit campaign</a>
+                    <a href={'#'} className="links-button">presskit</a>
+                    <a href={'#'} className="links-button">mission video</a>
                 </div>
-                <div className='footer'>
-                <div className='footer-links'>
-                <p className='footer-links-desc'>follow spacex |&nbsp;</p>
-                    <a href={'#'} className='footer-links-item'>twitter&nbsp;</a>
-                    <a href={'#'} className='footer-links-item'>youtube&nbsp;</a>
-                    <a href={'#'} className='footer-links-item'>flickr&nbsp;</a>
-                    <a href={'#'} className='footer-links-item'>instagram&nbsp;</a>
+                <div className="footer">
+                <div className="footer-links">
+                <span className="footer-links-desc">follow spacex |&nbsp;</span>
+                    <a href={'#'} className="footer-links-item">twitter&nbsp;</a>
+                    <a href={'#'} className="footer-links-item">youtube&nbsp;</a>
+                    <a href={'#'} className="footer-links-item">flickr&nbsp;</a>
+                    <a href={'#'} className="footer-links-item">instagram&nbsp;</a>
                 </div>
-                <div className='footer-copyright'>
+                <div className="footer-copyright">
                 <p>2018 Space Exploration Technologies Corp.</p></div>
                 </div>  
             </div>
