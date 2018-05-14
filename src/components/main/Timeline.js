49e56e9b92b0cd03348a28d3e0a27315f5c4e0da
ibaczewska <izabela.baczewska@gmail.com>
2018-05-14 20:29:04 +0200

    create line

--- src/components/main/Timeline.js
+++ src/components/main/Timeline.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Arrow from '../../assets/arrow_pointer.png';
+import launches from '../../assets/launches.json'
 
 
 
@@ -12,7 +14,9 @@ class Timeline extends React.Component {
                 <div className="timeline__dot"></div>
                     <div className="timeline__content">
                         <div className="timeline__date">12 May 2018</div>
-                        <span className="timeline__separator"></span>
+                        <span className="timeline__separator">
+                            <img className="timeline__separator__arrow" src={Arrow} />
+                        </span>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
                             <span className="timeline__rocket__value">Falcon 9 | </span>
@@ -26,7 +30,9 @@ class Timeline extends React.Component {
                 <div className="timeline__dot"></div>             
                     <div className="timeline__content">
                         <div className="timeline__date">12 May 2018</div>
-                        <div className="timeline__separator"></div>
+                        <span className="timeline__separator">
+                            <img className="timeline__separator__arrow" src={Arrow} />
+                        </span>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
                             <span className="timeline__rocket__value">Falcon 9 | </span>
@@ -39,7 +45,9 @@ class Timeline extends React.Component {
                 <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
                         <div className="timeline__date">12 May 2018</div>
-                        <div className="timeline__separator"></div>
+                        <span className="timeline__separator">
+                            <img className="timeline__separator__arrow" src={Arrow} />
+                        </span>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
                             <span className="timeline__rocket__value">Falcon 9 | </span>  
