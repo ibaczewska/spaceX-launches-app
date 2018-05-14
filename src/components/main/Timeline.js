896b8e2353d68f02c4f8302ea1c1333ccaed86d6
ibaczewska <izabela.baczewska@gmail.com>
2018-05-14 15:28:51 +0200

    timeline list

--- src/components/main/Timeline.js
+++ src/components/main/Timeline.js
@@ -9,12 +9,13 @@ class Timeline extends React.Component {
         return (
             <div className="timeline__wrapper">
                 <div className="timeline__block">
+                <div className="timeline__dot"></div>
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <span className="timeline__separator"></span>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span>
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
@@ -22,72 +23,78 @@ class Timeline extends React.Component {
                     </div>
                 </div>
                 <div className="timeline__block">
+                <div className="timeline__dot"></div>             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span>
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
                         <div></div>
                     </div>
                 </div><div className="timeline__block">
+                <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span>  
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
                         <div></div>
                     </div>
                 </div><div className="timeline__block">
+                <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span>  
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
                         <div></div>
                     </div>
                 </div><div className="timeline__block">
+                <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span> 
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
                         <div></div>
                     </div>
                 </div><div className="timeline__block">
+                <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value">Falcon 9 | </span> 
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
                         <div></div>
                     </div>
                 </div><div className="timeline__block">
+                <div className="timeline__dot"></div>                             
                     <div className="timeline__content">
-                        <h3 className="timeline__date">12 May 2018</h3>
-                        <div className="timeline__separator">--></div>
+                        <div className="timeline__date">12 May 2018</div>
+                        <div className="timeline__separator"></div>
                         <div className="timeline__rocket">
                             <span className="timeline__rocket__feature">Name:</span>
-                            <span className="timeline__rocket__value">Falcon 9</span> |
+                            <span className="timeline__rocket__value"> Falcon 9 | </span>
                             <span className="timeline__rocket__feature">Launch site:</span>
                             <span className="timeline__rocket__value">Lorem ipsum</span>
                         </div>
