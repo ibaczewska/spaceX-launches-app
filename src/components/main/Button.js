0ad0686681e39e22dbaca85dcf1ae9d67637bd28
ibaczewska <izabela.baczewska@gmail.com>
2018-05-27 21:05:56 +0200

    fetch api to button

--- src/components/main/Button.js
+++ src/components/main/Button.js
@@ -6,13 +6,20 @@ class Button extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+    state = {
+        launches: []
+    }
+    componentDidMount() {
+        fetch('https://api.spacexdata.com/v2/launches/all')
+        .then (res => res.json)
+        .then(json => this.setState({launches: json.rocket.rocket_name }));
+    }
     render() {
         const {rocket} = this.props;
         const { buttonText } = this.props;
         return (
 
-            <button href={'#'} className="button__item">{launches}</button>
+            <button href={'#'} className="button__item">{this.state.launches}</button>
 
         )
     }
--- src/components/main/Hero.js
+++ src/components/main/Hero.js
@@ -5,20 +5,10 @@ import Button from '../main/Button';
 import launches from '../../assets/launches.json';
 
 
-class Hero extends React.Component {
-    state = {
-        launches: []
-    }
-    async componentDidMount() {
-        const url ='https://api.spacexdata.com/v2/launches/all';
-        const result = await fetch(url);
-        const launches = await result.json();
-        this.setState({launches});
-        
-        // const filteredButtonsNames = 
-        //     this.setState({filteredButtonsNames});
-        //     console.log(this.state.filteredButtonsNames);
-    }
+export class Hero extends React.Component {
+
+   
+     
     render() {
     
         return (
@@ -30,14 +20,15 @@ class Hero extends React.Component {
                         <img className="hero__item__logo"src={heroLogo} width="400" alt="SpaceX logo"/>
                         <h4 className="hero__item__paragraph">Launches 2018</h4>
                         <div className="button__wrapper">
-                        {
+                        {/* {
                             this.state.launches.map( flight =>  (<Button key={flight.rocket.rocket_id} flight={flight}  />
                             ))
                         }
+                            */}
                            
-                            {/* <Button buttonText="Falcon 9"/>
+                            <Button  buttonText="Falcon 9"/>
                             <Button buttonText="Falcon Heavy"/>
-                            <Button buttonText="Dragon"/> */}
+                            <Button buttonText="Dragon"/>
                         </div>
                         
                         
